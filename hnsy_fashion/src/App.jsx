aee60d40a6817d9ab5d02ff557146055b62f9ef1
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2025-08-06 00:11:26 +0300

    Initial commit for Ecommerce Website

---
+++ hnsy_fashion/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello INSA Group-4: This is React App </h1> 
+      </div>
+      
+    </>
+  )
+}
+
+export default App
