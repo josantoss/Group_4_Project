b34ef7e4485b8dbe1d3c6bfe86bf8c23c21936ab
Hawi Sebsibe <hawisebsibe4@gmail.com>
2025-08-14 10:19:18 +0300

    Fix typo in contact controller's message field validation

--- AradaBuy/backend/controllers/contactController.js
+++ AradaBuy/backend/controllers/contactController.js
@@ -1,9 +1,12 @@
+const Contact = require("../models/Contact");
+
 exports.submitContactForm =async(req,res)=>{
     try{
         const{name,email,message}=req.body;
-        if (!name||!email||!messsage){
+        if (!name||!email||!message){
             return res.status(400).json({error:'All fields required'});
         }
+        const newContact=await Contact.create({name,email,message})
 
         res.status(200).json({message:'Form submitted successfully'});
     }catch(error){
